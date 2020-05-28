bf01bcaed5526613a598fa29a19ee725aa7c64bb
audywp <audy.wisudap@gmail.com>
2020-05-28 08:26:25 +0700

    Updates

---
+++ precache-manifest.b357c64c55f738de1cc2aca8400a529c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09eb6c301d2e56026d26d0c893f7f6e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c32a26afcb0dc0b458b",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fcfd3d84404e37fef473",
+    "url": "./static/css/main.509c31ac.chunk.css"
+  },
+  {
+    "revision": "0c32a26afcb0dc0b458b",
+    "url": "./static/js/2.eaec6d9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.eaec6d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcfd3d84404e37fef473",
+    "url": "./static/js/main.04bb5d1c.chunk.js"
+  },
+  {
+    "revision": "8b7c5296fab879150186",
+    "url": "./static/js/runtime-main.bb9f15b1.js"
+  },
+  {
+    "revision": "adc273cfe5ffb4db10a879b5ada1ed1d",
+    "url": "./static/media/nodata.adc273cf.png"
+  }
+]);
