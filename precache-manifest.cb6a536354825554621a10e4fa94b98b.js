44bc68980fcf281e4f3227886780fcd5cbe305bc
audywp <audy.wisudap@gmail.com>
2020-05-28 08:16:52 +0700

    Updates

---
+++ precache-manifest.cb6a536354825554621a10e4fa94b98b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f511485a78b46640eb158a86fa6f0d5",
+    "url": "/Teravin-front-end/index.html"
+  },
+  {
+    "revision": "0c32a26afcb0dc0b458b",
+    "url": "/Teravin-front-end/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e57db9b8064e7eed0305",
+    "url": "/Teravin-front-end/static/css/main.25f3e2e6.chunk.css"
+  },
+  {
+    "revision": "0c32a26afcb0dc0b458b",
+    "url": "/Teravin-front-end/static/js/2.eaec6d9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Teravin-front-end/static/js/2.eaec6d9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e57db9b8064e7eed0305",
+    "url": "/Teravin-front-end/static/js/main.967f55c9.chunk.js"
+  },
+  {
+    "revision": "17d5c79ef584ac011a65",
+    "url": "/Teravin-front-end/static/js/runtime-main.854e6814.js"
+  },
+  {
+    "revision": "adc273cfe5ffb4db10a879b5ada1ed1d",
+    "url": "/Teravin-front-end/static/media/nodata.adc273cf.png"
+  }
+]);
